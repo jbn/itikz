1b7a58931a708acbdf7ed06665feeee05bcb7054
ea42gh <ea42_github@mail.com>
2021-03-04 19:21:58 -0500

    fixed indexing bug in qr routine

--- itikz/nicematrix.py
+++ itikz/nicematrix.py
@@ -637,6 +637,8 @@ def ge( matrices, Nrhs=0, formater=repr, pivot_list=None, ref_path_list=None, co
 def qr(matrices, formater=repr, array_names=True, fig_scale=None, tmp_dir=None, keep_file=None):
     m = MatrixGridLayout( matrices, extra_rows = [1,0,0,0])
 
+    N = matrices[0][1].shape[1]
+
     m.array_format_string_list()
     m.array_of_tex_entries(formater=formater)
 
@@ -652,8 +654,8 @@ def qr(matrices, formater=repr, array_names=True, fig_scale=None, tmp_dir=None, 
 
     red      = make_decorator(text_color='red',  bf=True)
     red_rgt  = make_decorator(text_color='red',  bf=True, move_right=True)
-    m.add_row_above(0,2, [red(f'v_{i+1}')   for i in range(3)] + [red(f'w_{i+1}') for i in range(3)], formater= lambda a: a )
-    m.add_col_left( 1,1, [red_rgt(f'w^t_{i+1}') for i in range(3)], formater= lambda a: a )
+    m.add_row_above(0,2, [red(f'v_{i+1}')   for i in range(N)] + [red(f'w_{i+1}') for i in range(3)], formater= lambda a: a )
+    m.add_col_left( 1,1, [red_rgt(f'w^t_{i+1}') for i in range(N)], formater= lambda a: a )
 
     if array_names:
         dec = make_decorator(bf=True, delim='$')
